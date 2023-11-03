95f81fd87dc2ae6eb6bf6149a1012e88ca83e8b5
pammu453 <111453798+pammu453@users.noreply.github.com>
2023-11-03 16:46:50 +0530

    added httpOnly

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -33,7 +33,11 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } = validUser._doc
 
-        res.cookie('token', token).status(200).json(rest)
+        res.cookie('token', token, {
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true
+        }).status(200).json(rest)
 
     } catch (error) {
         next(error)
@@ -47,7 +51,11 @@ export const google = async (req, res, next) => {
         if (user) {
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
             const { password: pass, ...rest } = user._doc
-            res.cookie('token', token).status(200).json(rest)
+            res.cookie('token', token, {
+                sameSite: 'none',
+                secure: true,
+                httpOnly: true
+            }).status(200).json(rest)
 
         } else {
             const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8)
@@ -63,7 +71,11 @@ export const google = async (req, res, next) => {
             await newUser.save();
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
             const { password: pass, ...rest } = newUser._doc
-            res.cookie('token', token).status(200).json(rest)
+            res.cookie('token', token, {
+                sameSite: 'none',
+                secure: true,
+                httpOnly: true
+            }).status(200).json(rest)
         }
     } catch (error) {
         next(error)
